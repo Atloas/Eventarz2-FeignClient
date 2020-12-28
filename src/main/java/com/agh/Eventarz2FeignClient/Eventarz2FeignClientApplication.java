b4e65291e4e4997c059bf9f541231c6085fdc6b4
Atloas <miter010@gmail.com>
2020-12-28 22:32:14 +0100

    Simple Feign Client done

---
+++ src/main/java/com/agh/Eventarz2FeignClient/Eventarz2FeignClientApplication.java
@@ -0,0 +1,29 @@
+package com.agh.Eventarz2FeignClient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@EnableFeignClients
+@Controller
+public class Eventarz2FeignClientApplication {
+
+	@Autowired
+	private GreetingClient greetingClient;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eventarz2FeignClientApplication.class, args);
+	}
+
+	@RequestMapping("/get-greeting")
+	public String greeting(Model model) {
+		model.addAttribute("greeting", greetingClient.greeting());
+		return "greeting-view";
+	}
+
+}
---
+++ src/test/java/com/agh/Eventarz2FeignClient/Eventarz2FeignClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agh.Eventarz2FeignClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eventarz2FeignClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
